228652ba4e2a409f997dd929eb665ce6cc06b55f
SuZZZu <suzu,nkdca@gmail.com>
2018-09-19 17:22:24 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FileUploadModule } from 'ng2-file-upload';
+import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { AltaComponent } from './alta/alta.component';
+import { ConexionesComponent } from './conexiones/conexiones.component';
+import { NombreComponent } from './nombre/nombre.component';
+import { EstadoComponent } from './estado/estado.component';
+import { TecnologiaComponent } from './tecnologia/tecnologia.component';
+import { GruposComponent } from './grupos/grupos.component';
+import { PrioridadComponent } from './prioridad/prioridad.component';
+
+import { HttpModule } from '@angular/http';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaComponent,
+    ConexionesComponent,
+    NombreComponent,
+    EstadoComponent,
+    TecnologiaComponent,
+    GruposComponent,
+    PrioridadComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FileUploadModule
+    FileUploadModule,
+    routing,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AltaComponent } from './alta/alta.component';
+import { ConexionesComponent } from './conexiones/conexiones.component';
+import { EstadoComponent } from './estado/estado.component';
+import { GruposComponent } from './grupos/grupos.component';
+import { NombreComponent } from './nombre/nombre.component';
+import { PrioridadComponent } from './prioridad/prioridad.component';
+import { TecnologiaComponent } from './tecnologia/tecnologia.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+    { path: 'alta', component: AltaComponent },
+    { path: 'conectado', component: ConexionesComponent },
+    { path: 'estado', component: EstadoComponent },
+    { path: 'grupos', component: GruposComponent },
+    { path: 'nombre', component: NombreComponent },
+    { path: 'prioridad', component: PrioridadComponent},
+    { path: 'tecnologia', component: TecnologiaComponent },
+    { path: 'inicio', component: InicioComponent},
+    { path: 'login', component: LoginComponent }
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
