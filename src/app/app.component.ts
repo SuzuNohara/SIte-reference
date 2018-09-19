836103441148e1f5523f388bc6155817fe3d0c00
SuZZZu <suzu,nkdca@gmail.com>
2018-09-19 15:36:43 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  file:any;
+
+  fileChanged(e) {
+      this.file = e.target.files[0];
+      console.log('capturado');
+      let fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        console.log(fileReader.result);
+      }
+      fileReader.readAsText(this.file);
+  }
 }
