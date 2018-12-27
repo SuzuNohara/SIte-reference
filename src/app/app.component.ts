b637248f4a936467ace3144ca20e8345954ec25a
Suzu Nohara <suzu.nkdca@gmail.com>
2018-12-27 16:33:08 -0600

    Configuracion de sesionees y permisos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,41 +15,48 @@ export class AppComponent{
   userLoged: string;
   logoRef: string;
   key;
+  sessionRoot: boolean;
 
   @HostListener('document:keypress', ['$event']) handleKeyboardEvent(event: KeyboardEvent) { 
     this.key = event.key;
-    switch(this.key){
-      case 'a':
-        this.router.navigate(['/alta']);
-      break;
-      case 'o':
-        this.router.navigate(['/conectado']);
-      break;
-      case 'e':
-        this.router.navigate(['/estado']);
-      break;
-      case 'g':
-        this.router.navigate(['/grupos']);
-      break;
-      case 'n':
-        this.router.navigate(['/nombre']);
-      break;
-      case 'p':
-        this.router.navigate(['/prioridad']);
-      break;
-      case 't':
-        this.router.navigate(['/tecnologia']);
-      break;
+    if(this.activeSession){
+      switch(this.key){
+        case 'a':
+          this.router.navigate(['/alta']);
+        break;
+        case 'o':
+          this.router.navigate(['/conectado']);
+        break;
+        case 'e':
+          this.router.navigate(['/estado']);
+        break;
+        case 'g':
+          this.router.navigate(['/grupos']);
+        break;
+        case 'n':
+          this.router.navigate(['/nombre']);
+        break;
+        case 'p':
+          this.router.navigate(['/prioridad']);
+        break;
+        case 't':
+          if(this.sessionRoot){
+            this.router.navigate(['/tecnologia']);
+          }
+        break;
+      }
     }
   }
 
   constructor(private cookies: CookieService, private router: Router){
+    this.sessionRoot = false;
     this.logoRef = environment.AUTO_REFERENCE + "/img/sitios.png";
     this.router.navigate(['/']);
     this.userLoged = "";
     this.activeSession = false;
     this.sessionName = cookies.get(environment.SESSION_COOKIE);
-    if(this.sessionName.length > 0){  
+    if(this.sessionName.length > 0){
+      this.sessionRoot = this.sessionName == environment.USER_ROOT;
       this.activeSession = true;
       this.userLoged = this.sessionName;
       this.router.navigate(['/']);
